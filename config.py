bae50bb1da13022311aa42aa2f8b5c3c67eafd93
shyam gupta <shyamguptaofficial196@gmail.Com>
2025-08-21 10:51:14 +0200

    CHANGED README ORDER AND NAME OF PYTHON FILE.

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 API_ID = 22764918
 API_HASH = '90a36a816e024124d2b43241bef5a470'
 
-OUT_DIR = '../public_group_messages/'
-OUT_TRACKING = '../tracking/public_group.csv'
+OUT_DIR = 'public_group_messages/'
+OUT_TRACKING = 'tracking/public_group.csv'
 LIMIT_SEED_LIST = 100 # it can go in a config file
 MAX_MESSAGES = 20 # it can go in a config file
-SEED_LIST = '../seed/public_group_seed_list.txt'#'toxic5.csv' # it can go in a config file
+SEED_LIST = 'seed/public_group_seed_list.txt'#'toxic5.csv' # it can go in a config file
 
 
 
